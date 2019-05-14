9ded6db5643ef355166167341963fccb5ba15e4e
“lihaojie@quakoo.com <your_regislihaojie@quakoo.com>
2019-05-14 17:41:22 +0800

    加返回字段

--- store-system-common/src/main/java/com/store/system/client/ClientUserOnLogin.java
+++ store-system-common/src/main/java/com/store/system/client/ClientUserOnLogin.java
@@ -1,9 +1,13 @@
 package com.store.system.client;
 
+import com.store.system.model.Subordinate;
 import com.store.system.model.User;
+import com.store.system.service.SubordinateService;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.apache.commons.beanutils.BeanUtils;
+
+import javax.annotation.Resource;
 
 /**
  * 用户基本信息，包含登录token
@@ -12,8 +16,13 @@ import org.apache.commons.beanutils.BeanUtils;
 @Data
 public class ClientUserOnLogin extends User {
 
+    @Resource
+    private SubordinateService subordinateService;
     private String token;
 
+    private String subName;//门店名称
+
+    private String  sName;//公司名称
     public ClientUserOnLogin() {
     }
 
@@ -23,6 +32,18 @@ public class ClientUserOnLogin extends User {
             this.setPassword(null);
             this.setRand(-1);
             this.token = user.createToken();
+            if(user.getPsid()>0){
+               Subordinate subordinate =  subordinateService.load(user.getPsid());
+               if(subordinate!=null){
+                   this.setSName(subordinate.getName());
+               }
+            }
+            if(user.getSid()>0){
+                Subordinate subordinate = subordinateService.load(user.getSid());
+                if(subordinate!=null){
+                    this.setSubName(subordinate.getName());
+                }
+            }
         } catch (Exception e) {
             throw new IllegalStateException("user construction error!");
         }
