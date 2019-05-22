4c2bd894dda5fe034f4815add88e28626ca61937
zhaobiayng <185532426@qq.com>
2019-05-22 10:55:29 +0800

    pull

--- store-system-backend/src/main/webapp/backend_view/resource/js/lib/quakoo/Config.js
+++ store-system-backend/src/main/webapp/backend_view/resource/js/lib/quakoo/Config.js
@@ -24,11 +24,11 @@ var Config = (function(_super){
 
 
         /**测试服务器地址*/
-         // this.serverUrl = "http://39.107.247.82:20005";
+          this.serverUrl = "http://39.107.247.82:20005";
         /**李浩杰*/
         // this.serverUrl = "http://192.168.1.30:20005";
         /**马文军*/
-        this.serverUrl = "http://192.168.1.17:20005";
+        //this.serverUrl = "http://192.168.1.17:20005";
         /**聊天服务地址*/
         this.chatNativeUrl = "";
         /**图片服务器地址*/
