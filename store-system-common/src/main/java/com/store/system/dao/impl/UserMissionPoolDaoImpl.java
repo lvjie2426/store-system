d4c7d087ca16bf4106bb907ae45ccc12f3efc4fe
“lihaojie@quakoo.com <your_regislihaojie@quakoo.com>
2019-05-24 17:34:26 +0800

    Dao注解添加

--- store-system-common/src/main/java/com/store/system/dao/impl/UserMissionPoolDaoImpl.java
+++ store-system-common/src/main/java/com/store/system/dao/impl/UserMissionPoolDaoImpl.java
@@ -2,6 +2,8 @@ package com.store.system.dao.impl;
 
 import com.quakoo.space.CacheBaseDao;
 import com.quakoo.space.annotation.cache.CacheDaoMethod;
+import com.quakoo.space.annotation.dao.HyperspaceDao;
+import com.quakoo.space.enums.HyperspaceType;
 import com.quakoo.space.enums.cache.CacheMethodEnum;
 import com.store.system.dao.SubordinateMissionPoolDao;
 import com.store.system.dao.UserMissionPoolDao;
@@ -9,7 +11,7 @@ import com.store.system.model.UserMissionPool;
 
 import java.util.List;
 import java.util.Map;
-
+@HyperspaceDao(type = HyperspaceType.cache)
 public class UserMissionPoolDaoImpl extends CacheBaseDao<UserMissionPool> implements UserMissionPoolDao{
 
     @Override
