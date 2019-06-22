f273468ac85e127c4edff3292c3ea3926bfc3094
mawenjun <18699996375@163.com>
2019-06-22 16:01:32 +0800

    Signed-off-by: mawenjun <18699996375@163.com>

---
+++ store-system-backend/src/main/java/com/store/system/backend/Demo.java
@@ -0,0 +1,33 @@
+package com.store.system.backend;
+
+
+import com.store.system.util.ArithUtils;
+import com.store.system.util.TimeUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+/**
+ * @ClassName Demo
+ * @Description TODO
+ * @Author LaoMa
+ * @Date 2019/5/17 17:57
+ * @Version 1.0
+ **/
+public class Demo {
+
+    public static void main(String[] args) throws IllegalAccessException {
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//        GregorianCalendar calendar = new GregorianCalendar();
+//        calendar.set(Calendar.WEEK_OF_YEAR, calendar.get(Calendar.WEEK_OF_YEAR) - 1);
+//        String currentYearAndMonth = sdf.format(calendar.getTime());
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        List<Long> days = TimeUtils.getPastDays(calendar.get(Calendar.WEEK_OF_YEAR));//20190615 获得15
+
+        System.out.println(days);
+    }
+}
