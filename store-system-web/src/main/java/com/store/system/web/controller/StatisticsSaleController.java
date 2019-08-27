1aeed38b82b09122aeaef32f30353a7e7798f8c4
张猛 <zhangmeng@quakoo.com>
2019-08-27 16:07:39 +0800

    营销任务和顾客统计

--- store-system-web/src/main/java/com/store/system/web/controller/StatisticsSaleController.java
+++ store-system-web/src/main/java/com/store/system/web/controller/StatisticsSaleController.java
@@ -3,9 +3,11 @@ package com.store.system.web.controller;
 import com.google.common.collect.Lists;
 import com.quakoo.webframework.BaseController;
 import com.store.system.client.ClientSaleStatistics;
+import com.store.system.client.ClientStatisticsCustomer;
 import com.store.system.client.ResultClient;
 import com.store.system.exception.StoreSystemException;
 import com.store.system.service.SaleStatisticsService;
+import com.store.system.service.StatisticsCustomerJobService;
 import com.store.system.util.TimeUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +33,9 @@ public class StatisticsSaleController extends BaseController{
 
     @Resource
     private SaleStatisticsService saleStatisticsService;
+
+    @Resource
+    private StatisticsCustomerJobService statisticsCustomerJobService;
 
     //今日销售额统计
     @RequestMapping("/saleToday")
@@ -144,6 +149,68 @@ public class StatisticsSaleController extends BaseController{
                         res.add(statistics);
                     }
                 }
+            }
+            return this.viewNegotiating(request,response,new ResultClient(true,res));
+        }catch (StoreSystemException s){
+            return this.viewNegotiating(request,response,new ResultClient(false,s.getMessage()));
+        }
+    }
+
+
+    //本周注册用户人数 week当前时间戳
+    @RequestMapping("/statisticsByWeek")
+    public ModelAndView statisticsByWeek(HttpServletRequest request, HttpServletResponse response,String date,long subid)throws Exception{
+        try {
+            return this.viewNegotiating(request,response,new ResultClient(true,statisticsCustomerJobService.getCustomerCount(subid,date,1)));
+        }catch (StoreSystemException s){
+            return this.viewNegotiating(request,response,new ResultClient(false,s.getMessage()));
+        }
+    }
+    //本月  month当前时间戳
+    @RequestMapping("/statisticsByMonth")
+    public ModelAndView statisticsByMonth(HttpServletRequest request, HttpServletResponse response,String date,long subid)throws Exception{
+        try {
+            return this.viewNegotiating(request,response,new ResultClient(true,statisticsCustomerJobService.getCustomerCount(subid,date,2)));
+        }catch (StoreSystemException s){
+            return this.viewNegotiating(request,response,new ResultClient(false,s.getMessage()));
+        }
+    }
+
+    //近半年 year当前时间戳
+    @RequestMapping("/statisticsByHalfYear")
+    public ModelAndView statisticsByHalfYear(HttpServletRequest request, HttpServletResponse response,String date,long subid)throws Exception{
+        try {
+            return this.viewNegotiating(request,response,new ResultClient(true,statisticsCustomerJobService.getCustomerCount(subid,date,3)));
+        }catch (StoreSystemException s){
+            return this.viewNegotiating(request,response,new ResultClient(false,s.getMessage()));
+        }
+    }
+
+    //多店对比查询
+    @RequestMapping("/statisticsBysubids")
+    public ModelAndView statisticsBySubordinates(HttpServletRequest request,HttpServletResponse response,
+                                                 @RequestParam(name = "subids[]") List<Long> subids,String date,int type)throws Exception{
+        try {
+            List<ClientStatisticsCustomer> res = Lists.newArrayList();
+            for(Long subid:subids){
+                ClientStatisticsCustomer customerList = statisticsCustomerJobService.getCustomerCount(subid,date,type);
+                res.add(customerList);
+            }
+            return this.viewNegotiating(request,response,new ResultClient(true,res));
+        }catch (StoreSystemException s){
+            return this.viewNegotiating(request,response,new ResultClient(false,s.getMessage()));
+        }
+    }
+
+    //多店对比时间查询
+    @RequestMapping("/timeInterval")
+    public ModelAndView timeInterval(HttpServletRequest request,HttpServletResponse response,
+                                     @RequestParam(name = "subids[]") List<Long> subids, long startTime, long endTime)throws Exception{
+        try{
+            List<ClientStatisticsCustomer> res = Lists.newArrayList();
+            for(Long subid:subids){
+                ClientStatisticsCustomer customerList = statisticsCustomerJobService.getCustomerByTime(subid,startTime,endTime);
+                res.add((customerList));
             }
             return this.viewNegotiating(request,response,new ResultClient(true,res));
         }catch (StoreSystemException s){
