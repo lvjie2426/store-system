2ea7befa1844f9b65ecf77e59d775d2582cdadad
张猛 <zhangmeng@quakoo.com>
2019-08-27 16:18:28 +0800

    销售任务app

--- store-system-common/src/main/java/com/store/system/service/MissionService.java
+++ store-system-common/src/main/java/com/store/system/service/MissionService.java
@@ -4,7 +4,9 @@ package com.store.system.service;
 import com.quakoo.baseFramework.model.pagination.Pager;
 import com.store.system.client.ClientMission;
 import com.store.system.model.Mission;
+import com.store.system.model.User;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -21,6 +23,12 @@ public interface MissionService {
 
     public Map<String,Object> getAllMission(long sid)throws Exception;
 
+    public Map<String,Object> getAllMissionApp(long sid,Date date)throws Exception;
+
     //检查当前订单满足那些任务的完成条件并返回任务
     public List<Mission> checkMission(long skuId, long sid, long subid, long uid)throws Exception;
+
+    public Pager getWebByPager(Pager pager, long psid, int missionStatus, int type, int isNow)throws Exception;
+
+    public Pager getUserWebByPager(Pager pager, long psid, int missionStatus, int type, int isNow, User user)throws Exception;
 }
